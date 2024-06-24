cf5399800964126626a39cc45cf855f0b17c92f3
Jane Ibeh <janewhite.ji@gmail.com>
2024-06-24 10:09:28 +0100

    First code on dev branch

---
+++ src/constants/herodata.tsx
@@ -0,0 +1,38 @@
+
+
+export const herodata = [
+    {
+        _id: 1 ,
+        image: "/media/herofirstimage.png",
+        titleone: "Lessons and insights",
+        titletwo: "from 8 years",
+        paragraph: "Where to grow your business as a photographer: site or social media?",
+        button: "Register",
+    },
+
+    {
+        _id: 2 ,
+        image: "/media/herosecondimage.png",
+        titleone: "Building big business ",
+        titletwo: "A new guide",
+        paragraph: "Secrets of building and maintaining the foundation of a strong business",
+        button: "Register",
+
+    },
+
+    {
+        _id: 3 ,
+        image: "/media/herothirdimage.png",
+        titleone: "Ai in photography",
+        titletwo: "2024 tips",
+        paragraph: "Discover how artificial intelligence is revolutionizing photography and what it means for the future",
+        button: "Register",
+
+    },
+
+]
+
+
+
+// To avoid this next error;
+// Error: Failed to parse src "./media/herofirstimage.png" on next/image, when using relative image it must start with a leading slash "/" ie /media/herofirstimage.png 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,28 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    extend:{
+      fontFamily: {
+          'navfont': 'Inter',   
+      }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'neutral' :'#F5F7FA',
+      'lemon': '#4CAF4F',
+      'black':'#263238',
+      'textbl': '#18191F',
+      'grey':'#717171',
+      'gray':'#4D4D4D',
     },
   },
   plugins: [],
